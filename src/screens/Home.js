9971ed68798fea98b75a7e95bd59ff03750b6f9e
chathuraedirisingh <arkxstyler.forum@gmail.com>
2019-11-23 04:23:02 +0530

    cameraiew

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/Entypo';
 
 import { Platform } from 'react-native';
@@ -10,14 +10,18 @@ if (Platform.OS === 'android') {
 
 
 import colors from '../styles/colors';
-import { Header } from 'react-native-elements';
+import { Header, Image } from 'react-native-elements';
 import SearchBar from '../components/searchbar/SearchBar';
+
 
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 
 class HomePage extends Component {
     render() {
+        const { params } = this.props.navigation.state;
+        var image = params ? params.image : null;
+        // const encoded = image.base64;
         return (
             <View style={styles.container}>
                 <Header
@@ -25,18 +29,54 @@ class HomePage extends Component {
                 />
                 <View style={{ alignItems: 'center', justifyContent: 'center' }}>
                     <Text style={styles.name}>Good Morning Joe</Text>
-                    <TouchableOpacity onPress={this.props.navigation.openDrawer}>
+                    {/* <TouchableOpacity onPress={this.props.navigation.openDrawer}>
                         <Text>Open Drawer</Text>
-                    </TouchableOpacity>
+                    </TouchableOpacity> */}
+
+                    <View>{this.showImage(image)}</View>
+
                 </View>
             </View>
         );
     }
+
+    showImage(image) {
+        // console.log("Hi " + JSON.stringify(image));
+        if (image == null) {
+            return <Text>No</Text>
+        }
+        else {
+            const id = image.base64;
+            return <View style={styles.imageview}>
+                <Image
+                    source={{ uri: `data:image/gif;base64,${id}` }}
+                    style={styles.image}
+                />
+            </View>
+
+        }
+    }
+
 }
+
+const width = Dimensions.get('screen').width;
+const height = Dimensions.get('screen').height;
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         // flexDirection:'row',
+    },
+    image: {
+        width: width - 100,
+        height: height - 400,
+        resizeMode: 'cover',
+    },
+    imageview: {
+        shadowOpacity: 0.75,
+        shadowRadius: 5,
+        shadowColor: 'red',
+        shadowOffset: { height: 0, width: 0 },
     },
     SearchHeader: {
         backgroundColor: 'rgba(255,255,255,0.5)',
@@ -50,4 +90,16 @@ const styles = StyleSheet.create({
         color: 'rgba(0,0,0,0.5)',
     }
 });
+
+class ItemViewComponent extends Component {
+    render() {
+        const helloMessage = <Text> Hello, JSX! </Text>;
+        const goodbyeMessage = <Text> Goodbye, JSX! </Text>;
+        return (
+            <View>
+                {this.props.isGreeting ? helloMessage : goodbyeMessage}
+            </View>
+        );
+    }
+}
 export default HomePage;
