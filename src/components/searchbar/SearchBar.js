2fc80fd82cb5baa7d27f8896dd68a8f3981425a6
chathuraedirisingh <arkxstyler.forum@gmail.com>
2019-11-22 17:44:31 +0530

    2nd commit

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/Entypo';
+import Icn from 'react-native-vector-icons/Ionicons';
 import {
   View,
   Text,
   StyleSheet,
+  Dimensions
 } from 'react-native';
 import colors from '../../styles/colors';
 
+var width = Dimensions.get('window').width; //full width
 
 export default class SearchBar extends Component {
+    
   render() {
   	return (
     <View style={styles.wrapper}>
+        <Icon 
+            name="menu" 
+            size = {30}
+            color= {colors.white}
+            onPress={() => this.props.navigation.openDrawer()} />
+
       <View style={styles.searchContainer}>
-        <Icon
+        <Icn
           name="ios-search"
           size={20}
-          color={colors.gray02}
+          color={colors.white}
           style={styles.searchIcon}
         />
-        <Text style={styles.textInput}>
-Try "Cape Town"
-        </Text>
+        <Text style={styles.textInput}>Search</Text>
       </View>
     </View>
   	);
@@ -29,39 +37,38 @@ Try "Cape Town"
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    backgroundColor: 'rgba(255,255,255,0.9)',
-    width: '100%',
-    height: 80,
-    zIndex: 99,
-  },
+    wrapper:{
+        flex:1,
+        flexDirection:'row',
+        width:width-30
+    },
+
   searchContainer: {
-  	display: 'flex',
-  	borderWidth: 1,
-  	borderColor: colors.gray03,
-  	backgroundColor: colors.white,
-  	shadowColor: 'rgba(0,0,0,0.1)',
-  	shadowOffset: { width: 0, height: 5 },
-  	shadowOpacity: 0.7,
-  	shadowRadius: 10,
+  	// display: 'flex',
+      // borderWidth: 1,
+    // alignSelf: 'stretch',
+    width: width-90,
+  	// borderColor: colors.gray03,
+  	backgroundColor: 'rgba(255,255,255,0.5)',
+  	// shadowColor: 'rgba(0,0,0,0.1)',
+  	// shadowOffset: { width: 0, height: 5 },
+  	// shadowOpacity: 0.7,
+  	// shadowRadius: 10,
   	borderRadius: 3,
   	height: 40,
-  	marginTop: 28,
+  	// marginTop: 28,
   	marginLeft: 21,
-  	marginRight: 21,
+  	// marginRight: 21,
   },
   searchIcon: {
   	position: 'absolute',
-  	left: 18,
+  	left: 9,
   	top: 9,
   },
   textInput: {
     display: 'flex',
-    marginTop: 11,
-    marginLeft: 44,
-    color: colors.gray02,
+    marginTop: 9,
+    marginLeft: 30,
+    color: colors.white,
   },
 });
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
-import { Header } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Entypo';
 
 import { Platform } from 'react-native';
@@ -10,23 +9,31 @@ if (Platform.OS === 'android') {
 }
 
 import colors from '../styles/colors';
+import { Header } from 'react-native-elements';
+import SearchBar from '../components/searchbar/SearchBar';
 
 class HomePage extends Component {
+
+    static navigationOptions = {
+        drawerIcon: ({ tintColor }) => (
+            <Icon name="home" style={{ fontSize: 24, color: tintColor }} />
+        )
+    }
     render() {
         return (
             <View style={styles.container}>
                 <Header
-                    leftComponent={
-                        <Icon 
-                            name="menu" 
-                            size = {30}
-                            color= {colors.white}
-                            onPress={() => this.props.navigation.openDrawer()} />
-                    }
-                    centerComponent={<TextInput>Hi</TextInput>}
+                    // leftComponent={
+                    //     <Icon 
+                    //         name="menu" 
+                    //         size = {30}
+                    //         color= {colors.white}
+                    //         onPress={() => this.props.navigation.openDrawer()} />
+                    // }
+                    centerComponent={<SearchBar/>}
                 />
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text>Home Page</Text>
+                <View style={{  alignItems: 'center', justifyContent: 'center' }}>
+                    <Text style={styles.name}>Good Morning Joe</Text>
                 </View>
             </View>
         );
@@ -34,7 +41,19 @@ class HomePage extends Component {
 }
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        // flexDirection:'row',
+    },
+    SearchHeader:{
+        backgroundColor: 'rgba(255,255,255,0.5)',
+        borderRadius: 10,
+        alignItems: 'stretch',
+    },
+    name:{
+        marginTop:20,
+        fontSize: 30,
+        // fontWeight: 'bold',
+        color: 'rgba(0,0,0,0.5)',
     }
 });
 export default HomePage;
